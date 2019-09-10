10c984a2df98e76472ea4bb855861fb2730f215a
Esther Bernal <esther.bernal.lopez@gmail.com>
2019-09-10 16:39:46 +0200

    links

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const PlanetCard = props => {
   const { planet } = props
   console.log(planet)
   return (
-    <article className={`planet-card gallery-cell ${planet.name}`}>
-      <img src={`../images/${planet.name}.png`} alt={planet.name} />
-      <div className="planet-label">
-        <h3>{planet.name}</h3>
-      </div>
-    </article>
+    <Link to={`/planet/${planet.name}`} className={`planet-card gallery-cell`} >
+      <article className={`${planet.name}`}>
+        <img src={`../images/${planet.name}.png`} alt={planet.name} />
+        <div className="planet-label-line"></div>
+        <div className="planet-label">
+          <h3>{planet.name}</h3>
+        </div>
+      </article>
+    </Link>
   )
 }
 
