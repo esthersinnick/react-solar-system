a1c8f2b81bf1b769047b941be6600fe81c2b4093
Esther Bernal <esther.bernal.lopez@gmail.com>
2019-09-19 13:03:47 +0200

    descripciones planetas recortadas, back button

--- src/components/PlanetDetail.js
+++ src/components/PlanetDetail.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
 const PlanetDetail = props => {
   const { state } = props
+
+  const goToPreviousPage = () => {
+    props.history.goBack();
+  }
+
   return (
     <section>
       <article className="planet-detail">
-          <div className="planet-data">
-            <img src={`../images/${state.planet.name}.png`} alt={state.planet.name} />
-          </div>
-          <div className="planet-description">
-            <h3>{state.planet.name}</h3>
-            <p>{state.description}</p>
-            <button>Back</button>
-          </div>
+        <div className="planet-data">
+          <img src={`../images/${state.planet.name}.png`} alt={state.planet.name} />
+        </div>
+        <div className="planet-description">
+          <h3>{state.planet.name}</h3>
+          <p>{state.description}</p>
+          <button onClick={goToPreviousPage}>Back</button>
+        </div>
       </article>
     </section>
   )
 }
 
-export default PlanetDetail;
+export default withRouter(PlanetDetail);
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -8,25 +8,26 @@ const DetailPage = props => {
   const [state, setPlanet] = useState({});
   const { name } = props.match.params;
   useEffect(() => {
-    //solarSystemService.getData().then(planets => setPlanets(planets.data));
     const dataP = solarSystemService.getData().then(planets => planets.data);
     const descriptionP = wikiService.getPlanet(name);
     Promise.all([dataP, descriptionP])
-      .then(([data, description]) => {
+      .then(([data, descriptionData]) => {
         const planet = data.find(element => element.name === name)
+        const edited = descriptionData.substr(0, 300);
+        const index = edited.lastIndexOf('. ')
+        const description = edited.substr(0, index + 1);
         return { planet, description }
-        }
-      )
+      })
       .then(setPlanet)
   }, [])
 
   return (
     <div>
-      { 
-        state.planet && state.description ? 
+      {
+        state.planet && state.description ?
           <PlanetDetail state={state} />
-      :
-        <p>Loading...</p>
+          :
+          <p>Loading...</p>
       }
     </div>
   )
